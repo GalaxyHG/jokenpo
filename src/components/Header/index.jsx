5142d1852b3e99a9c32f8c3c738dd45c883e697f
Helena Rezende <99421906+GalaxyHG@users.noreply.github.com>
2024-08-09 16:35:17 +0000

    Header style, header links, react-router, routes work

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,17 @@
 import styles from './Header.module.css'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <>
-            <h1>This is header</h1>
+            <section>
+                <h1><span>CASUAL</span> GAMES</h1>
+                <ul>
+                    <li><Link className={styles.link} to="/">HOME</Link></li>
+                    <li><Link className={styles.link} to="/jokenpo">JOKENPÔ</Link></li>
+                    <li><Link className={styles.link} to="/tictactoe">TIC TAC TOE</Link></li>
+                </ul>
+            </section>
         </>
     )
 }
